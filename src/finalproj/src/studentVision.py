04a33cf63ee75a05f17a69ac70135084cd7fecd0
Sahej Singh <sahej3526@gmail.com>
2024-11-13 01:16:50 -0600

    renamed main to ctrl

--- src/finalproj/src/studentVision.py
+++ src/finalproj/src/studentVision.py
@@ -21,7 +21,9 @@ class lanenet_detector():
         self.bridge = CvBridge()
         # NOTE
         # Uncomment this line for lane detection of GEM car in Gazebo
-        self.sub_image = rospy.Subscriber('/gem/front_single_camera/front_single_camera/image_raw', Image, self.img_callback, queue_size=1)
+        # self.sub_image = rospy.Subscriber('/gem/front_single_camera/front_single_camera/image_raw', Image, self.img_callback, queue_size=1)
+        # Uncomment this line for lane detection for F1 car
+        self.sub_image = rospy.Subscriber('/D435I/color/image_raw', Image, self.img_callback, queue_size=1)
         # Uncomment this line for lane detection of videos in normal rosbag 0011, 0056
         # self.sub_image = rospy.Subscriber('camera/image_raw', Image, self.img_callback, queue_size=1)
         # Uncomment this line for lane detection of videos in larger rosbag 830
